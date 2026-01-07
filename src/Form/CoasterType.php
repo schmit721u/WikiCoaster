2fa7ed496447a639937939b44c43d7625fb26ff0
Lana <lana.schmitt9@etu.lorraine-univ.fr>
2026-01-07 09:30:35 +0100

    ajout de autorizationchecker

--- src/Form/CoasterType.php
+++ src/Form/CoasterType.php
@@ -8,10 +8,16 @@ use App\Entity\Park;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CoasterType extends AbstractType
 {
+
+    public function __construct(
+        private AuthorizationCheckerInterface $autorizationChecker,
+     ) {}
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -35,6 +41,12 @@ class CoasterType extends AbstractType
                 'by_reference' => false,
             ])
         ;
+
+        if  ($this->autorizationChecker->isGranted('ROLE_ADMIN')) {
+            $builder->add('published', options :[
+                'label'=>"Publier l'affiche" ,
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
